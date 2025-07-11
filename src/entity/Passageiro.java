0555eef23194d17994e95d8add0143c9c7ccbebe
Difierro <gustavo7867grdr@gmail.com>
2025-07-11 16:02:31 -0300

    feat: add listarPassageiro() e getters e setters

--- src/entity/Passageiro.java
+++ src/entity/Passageiro.java
@@ -1,22 +1,24 @@
 package entity;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.Collection;
+import java.util.List;
 
-import conexao.Conexao;
+import DAO.PassageiroDAO;
 
 public class Passageiro extends Pessoa {
     private int idPassageiro;
     private String cartaoDados;
 
     public Passageiro(String nome, String cpf, String email, String telefone, String senha, String genero,
-            int idPassageiro, String cartaoDados) {
+            int idPassageiro, String cartaoDados, int quantidadeCorridas, float avaliacaoMedia) {
         super(nome, cpf, email, telefone, senha, genero);
         this.idPassageiro = idPassageiro;
         this.cartaoDados = cartaoDados;
+        super.setAvaliacaoMedia(avaliacaoMedia);
+        super.setQuantidadeCorridas(quantidadeCorridas);
+    }
+
+    public Passageiro(String nome, String cpf, String email, String telefone, String senha, String genero){
+        super(nome, cpf, email, telefone, senha, genero);
+        cartaoDados = null;
     }
 
     public boolean realizarPagamento(float valor, String metodoPagamento){
@@ -25,29 +27,28 @@ public class Passageiro extends Pessoa {
     }
 
     public static void listarPassageiros(){
-        String sql = "SELECT * FROM passageiro";
-
-        Collection<Passageiro> passageiros = new ArrayList<>();
-         
-        try(Connection conn = Conexao.getConnection();
-            PreparedStatement ps = conn.prepareStatement(sql)){
-                ResultSet rs = ps.executeQuery();
-
-                while (rs.next()) {
-                    Passageiro p = new Passageiro(rs.getString("nome"), rs.getString("cpf"), rs.getString("email"), rs.getString("telefone"), rs.getString("senha"), rs.getString("genero"), rs.getInt("idpassageiro"), rs.getString("cartaodados"));
-
-                    passageiros.add(p);
-                }
-        }catch (SQLException e) {
-            System.out.println("erro ao buscar passageiros" + e.getMessage());
-        }
-
+        PassageiroDAO dao = new PassageiroDAO();
+        List<Passageiro> passageiros = dao.listarPassageiros();
+        System.out.printf("| %-3s | %-30s | %-30s | %-16s | %-10s | %-22s | %-9s |\n",
+        "ID", "Nome", "Email", "Telefone", "Gênero", "Quantidade de Corridas", "Avaliação");
+        System.out.println("|-----|--------------------------------|--------------------------------|------------------|------------|------------------------|-----------|");
         for (Passageiro passageiro : passageiros) {
-            System.out.println(passageiro);
+            System.out.printf("| %-3d | %-30s | %-30s | %-16s | %-10s | %-22d | %-9.2f |\n",
+                    passageiro.getIdPassageiro(),
+                    passageiro.getNome(),
+                    passageiro.getEmail(),
+                    passageiro.getTelefone(),
+                    passageiro.getGenero(),
+                    passageiro.getQuantidadeCorridas(),
+                    passageiro.getAvaliacaoMedia());
         }
     }
 
-    public static void main(String[] args) {
-        Passageiro.listarPassageiros();
+    public int getIdPassageiro() {
+        return idPassageiro;
+    }
+
+    public String getCartaoDados() {
+        return cartaoDados;
     }
 }
